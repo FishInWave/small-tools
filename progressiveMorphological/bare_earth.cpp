e266d46911cc4c9d3e72e0057784b53e7c2b2b6e
yuwei Xu <850565178@qq.com>
2020-09-08 16:37:38 +0800

    add cluster and remove floor

--- progressiveMorphological/bare_earth.cpp
+++ progressiveMorphological/bare_earth.cpp
@@ -3,50 +3,60 @@
 #include <pcl/filters/extract_indices.h>
 #include <pcl/segmentation/progressive_morphological_filter.h>
 
-int
-main (int argc, char** argv)
+int main(int argc, char **argv)
 {
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PointIndicesPtr ground (new pcl::PointIndices);
+  std::string pcd_name;
+  if (argc >= 2)
+  {
+    pcd_name = argv[1];
+  }else
+  {
+    PCL_WARN("Youi must add a pcd file name and an out name");
+    return 0;
+  }
+  
+  
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointIndicesPtr ground(new pcl::PointIndices);
 
   // Fill in the cloud data
   pcl::PCDReader reader;
   // Replace the path below with the path where you saved your file
-  reader.read<pcl::PointXYZ> ("samp11-utm.pcd", *cloud);
+  reader.read<pcl::PointXYZ>(pcd_name, *cloud);
 
   std::cerr << "Cloud before filtering: " << std::endl;
   std::cerr << *cloud << std::endl;
 
   // Create the filtering object
   pcl::ProgressiveMorphologicalFilter<pcl::PointXYZ> pmf;
-  pmf.setInputCloud (cloud);
-  pmf.setMaxWindowSize (20);
-  pmf.setSlope (1.0f);
-  pmf.setInitialDistance (0.5f);
-  pmf.setMaxDistance (3.0f);
-  pmf.extract (ground->indices);
+  pmf.setInputCloud(cloud);
+  pmf.setMaxWindowSize(5);
+  pmf.setSlope(1.0f);
+  pmf.setInitialDistance(0.1f);
+  pmf.setMaxDistance(0.5f);
+  pmf.extract(ground->indices);
 
   // Create the filtering object
   pcl::ExtractIndices<pcl::PointXYZ> extract;
-  extract.setInputCloud (cloud);
-  extract.setIndices (ground);
-  extract.filter (*cloud_filtered);
+  extract.setInputCloud(cloud);
+  extract.setIndices(ground);
+  extract.filter(*cloud_filtered);
 
   std::cerr << "Ground cloud after filtering: " << std::endl;
   std::cerr << *cloud_filtered << std::endl;
 
   pcl::PCDWriter writer;
-  writer.write<pcl::PointXYZ> ("samp11-utm_ground.pcd", *cloud_filtered, false);
+  writer.write<pcl::PointXYZ>("samp11-utm_ground.pcd", *cloud_filtered, false);
 
   // Extract non-ground returns
-  extract.setNegative (true);
-  extract.filter (*cloud_filtered);
+  extract.setNegative(true);
+  extract.filter(*cloud_filtered);
 
   std::cerr << "Object cloud after filtering: " << std::endl;
   std::cerr << *cloud_filtered << std::endl;
 
-  writer.write<pcl::PointXYZ> ("samp11-utm_object.pcd", *cloud_filtered, false);
+  writer.write<pcl::PointXYZ>("samp11-utm_object.pcd", *cloud_filtered, false);
 
   return (0);
 }
